9eef74fb731f966e9b3b8eb2f59e707eebde7bb4
Eric Curley <Eric.Curley94@gmail.com>
2020-08-15 12:46:34 -0400

    Create Game Window

--- main.py
+++ main.py
@@ -2,3 +2,28 @@ import pygame as pg
 import sys
 from settings import *
 from sprites import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+    def new(self):
+        pass
+
+    def run(self):
+        pass
+    
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
