0a22f638625459b4f90396ba419352be56c48cf1
Eric Curley <Eric.Curley94@gmail.com>
2020-08-15 12:53:51 -0400

    More structure

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
-        self.load_data()
+        self.clock = pg.time.Clock() #create clock
+        pg.key.set_repeat(500, 100) #key repeate delay
+        self.load_data() #load level data eventually
 
     def load_data(self):
         pass
@@ -19,6 +21,25 @@ class Game:
         self.player = Player(self, 10, 10)
     
     def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000 #still not sure
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+    
+    def update(self):
+        pass
+    
+    def events(self):
+        pass
+
+    def draw(self):
         pass
     
     def show_start_screen(self):
