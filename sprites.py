2c025c023b38a72b4c2a1ac05c08dd36e3892ad5
Eric Curley <Eric.Curley94@gmail.com>
2020-08-15 12:49:38 -0400

    Define player

---
+++ sprites.py
@@ -0,0 +1,13 @@
+import pygame as pg
+from settings import *
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
