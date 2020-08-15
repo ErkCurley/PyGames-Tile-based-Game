3af89240c6b3f7acfc80fecd719910d6d0b4243d
Eric Curley <Eric.Curley94@gmail.com>
2020-08-15 13:07:28 -0400

    Update project to ep1 status

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# KidsCanCode - Game Development with Pygame video series
+# Tile-based game - Part 1
+# Project setup
+# Video link: https://youtu.be/3UxnelT9aCo
 import pygame as pg
 import sys
 from settings import *
@@ -8,23 +12,26 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock() #create clock
-        pg.key.set_repeat(500, 100) #key repeate delay
-        self.load_data() #load level data eventually
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
     def load_data(self):
         pass
 
     def new(self):
+        # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
         self.player = Player(self, 10, 10)
-    
+        for x in range(10, 20):
+            Wall(self, x, 5)
+
     def run(self):
         # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000 #still not sure
+            self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
@@ -32,19 +39,40 @@ class Game:
     def quit(self):
         pg.quit()
         sys.exit()
-    
+
     def update(self):
+        # update portion of the game loop
         self.all_sprites.update()
-    
-    def events(self):
-        pass
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
         self.screen.fill(BGCOLOR)
-        # self.draw_grid()
-        # self.all_sprites.draw(self.screen)
-        # pg.display.flip()
-    
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
     def show_start_screen(self):
         pass
 
