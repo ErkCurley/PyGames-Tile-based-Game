7ae4b54f5385e4f9fc637deecd0f16a6ff3ee82a
Eric Curley <Eric.Curley94@gmail.com>
2020-08-27 20:25:02 -0400

    Created a tiled map

---
+++ maps/Cave.tsx
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Cave" tilewidth="16" tileheight="16" tilecount="336" columns="16">
+ <image source="img/cave.png" width="256" height="336"/>
+ <terraintypes>
+  <terrain name="Water Rock" tile="17"/>
+  <terrain name="Lava Rock" tile="193"/>
+  <terrain name="Void Rock" tile="103"/>
+  <terrain name="Light Lava" tile="307"/>
+  <terrain name="Heavy Lava" tile="54"/>
+  <terrain name="Rocky Top Bits" tile="54"/>
+  <terrain name="Transparent Top Bits" tile="54"/>
+  <terrain name="Lava Top Bits" tile="54"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="6" terrain="5,5,5,"/>
+ <tile id="7" terrain="5,5,,5"/>
+ <tile id="12" terrain=",5,,"/>
+ <tile id="13" terrain="5,5,,"/>
+ <tile id="14" terrain="5,5,,"/>
+ <tile id="15" terrain="5,,,"/>
+ <tile id="16" terrain=",0,,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="22" terrain="5,,5,"/>
+ <tile id="23" terrain=",5,,5"/>
+ <tile id="24" terrain="5,,5,"/>
+ <tile id="25" terrain=",5,,5"/>
+ <tile id="26" terrain="6,,6,"/>
+ <tile id="27" terrain=",6,,6"/>
+ <tile id="32" terrain=",0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,,,"/>
+ <tile id="38" terrain="5,,5,"/>
+ <tile id="39" terrain=",5,,5"/>
+ <tile id="40" terrain="5,,5,"/>
+ <tile id="41" terrain=",5,,5"/>
+ <tile id="42" terrain="6,,6,"/>
+ <tile id="43" terrain=",6,,6"/>
+ <tile id="48" terrain=",0,0,0"/>
+ <tile id="49" terrain="0,,0,0"/>
+ <tile id="54" terrain="5,5,5,5"/>
+ <tile id="59" terrain="5,,5,5"/>
+ <tile id="60" terrain=",,,5"/>
+ <tile id="61" terrain=",,5,5"/>
+ <tile id="62" terrain=",,5,5"/>
+ <tile id="63" terrain=",,5,"/>
+ <tile id="64" terrain="0,0,,0"/>
+ <tile id="65" terrain="0,0,0,"/>
+ <tile id="75" terrain=",5,5,5"/>
+ <tile id="76" terrain=",6,,"/>
+ <tile id="77" terrain="6,6,,"/>
+ <tile id="78" terrain="6,6,,"/>
+ <tile id="79" terrain="6,,,"/>
+ <tile id="80" terrain=",,0,0"/>
+ <tile id="81" terrain=",,0,0"/>
+ <tile id="82" terrain=",,0,0"/>
+ <tile id="86" terrain=",,,2"/>
+ <tile id="87" terrain=",,2,2"/>
+ <tile id="88" terrain=",,2,"/>
+ <tile id="96" terrain="0,0,,"/>
+ <tile id="97" terrain="0,0,,"/>
+ <tile id="98" terrain="0,0,,"/>
+ <tile id="102" terrain=",2,,2"/>
+ <tile id="103" terrain="2,2,2,2"/>
+ <tile id="104" terrain="2,,2,"/>
+ <tile id="118" terrain=",2,,"/>
+ <tile id="119" terrain="2,2,,"/>
+ <tile id="120" terrain="2,,,"/>
+ <tile id="134" terrain=",2,2,2"/>
+ <tile id="135" terrain="2,,2,2"/>
+ <tile id="150" terrain="2,2,,2"/>
+ <tile id="151" terrain="2,2,2,"/>
+ <tile id="176" terrain=",,,1"/>
+ <tile id="177" terrain=",,1,1"/>
+ <tile id="178" terrain=",,1,"/>
+ <tile id="179" terrain="7,,7,"/>
+ <tile id="180" terrain=",7,,7"/>
+ <tile id="182" terrain=",2,,2"/>
+ <tile id="183" terrain="2,,2,"/>
+ <tile id="192" terrain=",1,,1"/>
+ <tile id="193" terrain="1,1,1,1"/>
+ <tile id="194" terrain="1,,1,"/>
+ <tile id="195" terrain="7,,7,"/>
+ <tile id="196" terrain=",7,,7"/>
+ <tile id="208" terrain=",1,,"/>
+ <tile id="209" terrain="1,1,,"/>
+ <tile id="210" terrain="1,,,"/>
+ <tile id="211" terrain="7,,7,"/>
+ <tile id="212" terrain=",7,,7"/>
+ <tile id="224" terrain=",1,1,1"/>
+ <tile id="225" terrain="1,,1,1"/>
+ <tile id="227" terrain="7,,7,"/>
+ <tile id="228" terrain=",7,,7"/>
+ <tile id="240" terrain="1,1,,1"/>
+ <tile id="241" terrain="1,1,1,"/>
+ <tile id="242" terrain=",7,,"/>
+ <tile id="243" terrain="7,7,,"/>
+ <tile id="244" terrain="7,7,,"/>
+ <tile id="245" terrain="7,,,"/>
+ <tile id="304" terrain="3,3,3,3"/>
+ <tile id="305" terrain="3,3,3,3"/>
+ <tile id="306" terrain="4,4,4,4"/>
+ <tile id="307" terrain="4,4,4,4"/>
+ <tile id="308" terrain="4,4,4,4"/>
+ <tile id="309" terrain="4,4,4,4"/>
+ <tile id="320" terrain="3,3,3,3"/>
+ <tile id="321" terrain="3,3,3,3"/>
+ <tile id="322" terrain="4,4,4,4"/>
+ <tile id="323" terrain="4,4,4,4"/>
+ <tile id="324" terrain="4,4,4,4"/>
+ <tile id="325" terrain="4,4,4,4"/>
+</tileset>
